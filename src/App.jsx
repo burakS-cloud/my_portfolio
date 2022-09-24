3889cfc12ca1ccec1d8ac57e9ab6735b2f7b4d28
burakS-cloud <bsurumcuoglu@gmail.com>
2022-09-24 17:32:33 +0300

    modified strict mode

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import Home from "./routes/Home";
 import Contact from "./routes/Contact";
 import Projects from "./routes/Projects";
 import About from "./routes/About";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </>
   );
 }
